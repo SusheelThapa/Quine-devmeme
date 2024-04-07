07b1d3c2377f95aad9998d0c4173c41e32111304
SusheelThapa <susheelthapa90@gmail.com>
2024-04-07 21:26:06 +0545

    setup route for ai-joke generator

---
+++ src/pages/AiJokeGenerator/AiJokeGenerator.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+const AiJokeGenerator = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
+
+export default AiJokeGenerator;
